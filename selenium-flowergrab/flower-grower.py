3bf25eef95d7721e1df8cab6676932a6089d5794
Jack Rickards <jack.rickards@hotmail.co.uk>
2021-10-17 15:40:47 +0100

    disable gpu to potentially fix issue on po

--- selenium-flowergrab/flower-grower.py
+++ selenium-flowergrab/flower-grower.py
@@ -23,7 +23,7 @@ chrome_options.add_argument("--headless")
 prefs = {'download.default_directory' : chrome_download_dir}
 chrome_options.add_experimental_option('prefs', prefs)
 # chrome_options.add_argument("--no-sandbox")
-#chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--disable-gpu")
 
 # make sure to install the driver first and put
 # it in the same directory
