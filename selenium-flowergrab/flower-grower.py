1bef59c50ca91f84b617b975e35d4fce63a5d8ae
Jack Rickards <jack.rickards@hotmail.co.uk>
2021-10-17 14:28:02 +0100

    added prints throughout to test speed of pi

--- selenium-flowergrab/flower-grower.py
+++ selenium-flowergrab/flower-grower.py
@@ -4,10 +4,12 @@ selenium fine on the pi
 If we can then we can move onto bigger things
 """
 
+print("importing libraries...")
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 # run headless
+print("setting options...")
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 #chrome_options.add_argument("--disable-gpu")
@@ -15,9 +17,11 @@ chrome_options.add_argument("--headless")
 # make sure to install the driver first and put
 # it in the same directory
 #driver = webdriver.Chrome("./chromedriver", options=chrome_options)
+print("initialising driver")
 driver = webdriver.Chrome(options=chrome_options)
 
 # load a webpage
+print("loading webpage...")
 driver.get("https://www.python.org")
 
 # should be "Welcome to Python.org"
