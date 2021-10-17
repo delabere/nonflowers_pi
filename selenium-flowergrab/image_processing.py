1b288f81cabc732825f3bc4301d842709e3f015c
Jack Rickards <jack.rickards@hotmail.co.uk>
2021-10-17 14:19:26 +0100

    image again

--- selenium-flowergrab/image_processing.py
+++ selenium-flowergrab/image_processing.py
@@ -7,9 +7,11 @@ img = Image.open(file)
 
 #converter = PIL.ImageEnhance.Color(img)
 converter = PIL.ImageEnhance.Sharpness(img)
-img2 = converter.enhance(15)
+img2 = converter.enhance(8)
+colour_converter = PIL.ImageEnhance.Color(img2)
+img2 = colour_converter.enhance(1.5)
 
 
 #img.show()
 img2.show()
-img2.save("resized_sharp_as_fuck.png")
+img2.save("manual_downloads/resized_sharp_as_fuck_with_colour.png")
