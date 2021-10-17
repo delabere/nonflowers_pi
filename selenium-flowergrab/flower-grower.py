838a2e5d58454e585e009f9cdbacf2a5a2e9e47f
Jack Rickards <jack.rickards@hotmail.co.uk>
2021-10-17 16:12:59 +0100

    retry logic

--- selenium-flowergrab/flower-grower.py
+++ selenium-flowergrab/flower-grower.py
@@ -21,6 +21,17 @@ prefs = {'download.default_directory' : chrome_download_dir}
 chrome_options.add_experimental_option('prefs', prefs)
 # chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-gpu")
+# # // ChromeDriver is just AWFUL because every version or two it breaks unless you pass cryptic arguments
+# //AGRESSIVE: options.setPageLoadStrategy(PageLoadStrategy.NONE); // https://www.skptricks.com/2018/08/timed-out-receiving-message-from-renderer-selenium.html
+chrome_options.add_argument("start-maximized")# // https://stackoverflow.com/a/26283818/1689770
+chrome_options.add_argument("enable-automation")# // https://stackoverflow.com/a/43840128/1689770
+chrome_options.add_argument("--headless")# // only if you are ACTUALLY running headless
+chrome_options.add_argument("--no-sandbox")# //https://stackoverflow.com/a/50725918/1689770
+chrome_options.add_argument("--disable-infobars")# //https://stackoverflow.com/a/43840128/1689770
+chrome_options.add_argument("--disable-dev-shm-usage")# //https://stackoverflow.com/a/50725918/1689770
+chrome_options.add_argument("--disable-browser-side-navigation")# //https://stackoverflow.com/a/49123152/1689770
+chrome_options.add_argument("--disable-gpu")# //https://stackoverflow.com/questions/51959986/how-to-solve-selenium-chromedriver-timed-out-receiving-message-from-renderer-exc
+
 
 # make sure to install the driver first and put
 # it in the same directory
@@ -36,7 +47,17 @@ print("waiting to make sure page has loaded...")
 # time.sleep(20)
 driver.save_screenshot("screenshot.png")
 print("executing script...")
-driver.execute_script("makeDownload()")
+
+# seems to fail consistently on the first time so we should retry
+max_retries = 5
+retries = 0
+while retries < max_retries:
+    try:
+        driver.execute_script("makeDownload()")
+        break
+    except:
+        pass
+
 print("waiting to make sure file has downloaded...")
 time.sleep(5)
 # should be "Welcome to Python.org"
