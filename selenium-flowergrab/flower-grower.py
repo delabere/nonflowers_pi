3ac630665e86b626d5f958186f79f644b372a4bc
Jack Rickards <jack.rickards@hotmail.co.uk>
2021-10-17 16:43:43 +0100

    refactored flower grower to make it much more readable

--- selenium-flowergrab/flower-grower.py
+++ selenium-flowergrab/flower-grower.py
@@ -1,66 +1,63 @@
-"""
-This file is just a test to make sure that we can run
-selenium fine on the pi
-If we can then we can move onto bigger things
-"""
-
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 import time
 import os
 
-# run headless
-print("setting options...")
-chrome_options = Options()
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-chrome_download_dir = r'C:\Users\jackr\Desktop\projects\nonflowers_pi\selenium-flowergrab\\'
-chrome_download_dir = os.getcwd()
+def get_flower():
+    """this function is only called when the file is executed directly"""
 
-chrome_options.add_argument("--headless")
-prefs = {'download.default_directory' : chrome_download_dir}
-chrome_options.add_experimental_option('prefs', prefs)
-# chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-gpu")
-# # // ChromeDriver is just AWFUL because every version or two it breaks unless you pass cryptic arguments
-# //AGRESSIVE: options.setPageLoadStrategy(PageLoadStrategy.NONE); // https://www.skptricks.com/2018/08/timed-out-receiving-message-from-renderer-selenium.html
-chrome_options.add_argument("start-maximized")# // https://stackoverflow.com/a/26283818/1689770
-chrome_options.add_argument("enable-automation")# // https://stackoverflow.com/a/43840128/1689770
-chrome_options.add_argument("--headless")# // only if you are ACTUALLY running headless
-chrome_options.add_argument("--no-sandbox")# //https://stackoverflow.com/a/50725918/1689770
-chrome_options.add_argument("--disable-infobars")# //https://stackoverflow.com/a/43840128/1689770
-chrome_options.add_argument("--disable-dev-shm-usage")# //https://stackoverflow.com/a/50725918/1689770
-chrome_options.add_argument("--disable-browser-side-navigation")# //https://stackoverflow.com/a/49123152/1689770
-chrome_options.add_argument("--disable-gpu")# //https://stackoverflow.com/questions/51959986/how-to-solve-selenium-chromedriver-timed-out-receiving-message-from-renderer-exc
+    print("setting options...")
+    chrome_options = Options()
+
+    # this sets the download directory for the flower image to be
+    # ...wherever we run the script from
+    prefs = {'download.default_directory' : os.getcwd()}
+    chrome_options.add_experimental_option('prefs', prefs)
+
+    # this argument tells the browser to run without any gui
+    # ...which is essential on the pi zero
+    chrome_options.add_argument("--headless")
+
+    # these options are here to stop the timeout bug but do not seem to work
+    # ...going to leave them here but comment them in case we need them again
+    # chrome_options.add_argument("start-maximized")# // https://stackoverflow.com/a/26283818/1689770
+    # chrome_options.add_argument("enable-automation")# // https://stackoverflow.com/a/43840128/1689770
+    # chrome_options.add_argument("--headless")# // only if you are ACTUALLY running headless
+    # chrome_options.add_argument("--no-sandbox")# //https://stackoverflow.com/a/50725918/1689770
+    # chrome_options.add_argument("--disable-infobars")# //https://stackoverflow.com/a/43840128/1689770
+    # chrome_options.add_argument("--disable-dev-shm-usage")# //https://stackoverflow.com/a/50725918/1689770
+    # chrome_options.add_argument("--disable-browser-side-navigation")# //https://stackoverflow.com/a/49123152/1689770
+    # chrome_options.add_argument("--disable-gpu")# //https://stackoverflow.com/questions/51959986/how-to-solve-selenium-chromedriver-timed-out-receiving-message-from-renderer-exc
 
 
-# make sure to install the driver first and put
-# it in the same directory
-print("initialising driver...")
-driver = webdriver.Chrome(options=chrome_options)
-# driver = webdriver.Chrome("./chromedriver", options=chrome_options)
+    # make sure to install the driver first and put
+    # ...it in the same directory
+    print("initialising driver...")
+    driver = webdriver.Chrome(options=chrome_options)
 
-# load a webpage
-print("loading webpage...")
-# driver.get("https://www.python.org")
-driver.get("http://nonflowers.lingdong.works/")
-print("waiting to make sure page has loaded...")
-# time.sleep(20)
-driver.save_screenshot("screenshot.png")
-print("executing script...")
-
-# seems to fail consistently on the first time so we should retry
-max_retries = 5
-retries = 0
-while retries < max_retries:
+    # load the webpage
+    print("loading webpage...")
+    # although this will timeout - it seems that
+    # ...enough of the page does load for it to not matter
+    # TODO: can we make this timeout sooner? or finish on loading the element we care about?
+    # ...this could save some time (precious on the low power pi)
     try:
         driver.execute_script("makeDownload()")
     except:
-        pass
+        print("timed out - continuing regardless")
 
-print("waiting to make sure file has downloaded...")
-time.sleep(5)
-# should be "Welcome to Python.org"
-title = driver.title
+    # this runs the `makeDownload()` function from index.js
+    # ...which downloads the flower image locally
+    driver.execute_script("makeDownload()")
 
-# close
-driver.close()
+    print("waiting to make sure file has downloaded...")
+    time.sleep(5)
+    # should be "Welcome to Python.org"
+    title = driver.title
+
+    # close
+    driver.close()
+
+if __name__ == "__main__":
+    get_flower()
