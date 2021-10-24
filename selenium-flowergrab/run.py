edae6d0b255730bc51ca45f35e92ab1e6aff1b7f
Jack Rickards <jack.rickards@hotmail.co.uk>
2021-10-24 17:47:06 +0100

    added some notes

--- selenium-flowergrab/run.py
+++ selenium-flowergrab/run.py
@@ -2,6 +2,10 @@ from process_image import process_image
 from flower_grower import get_flower
 from display_image import display_image
 
+"""
+On the raspberry Pi Zero this process takes ~8 minutes
+Most of that is in the get_flower function
+"""
 if __name__ == "__main__":
     get_flower()     # get new flower image
     process_image()  # process that into something which looks better on screen
