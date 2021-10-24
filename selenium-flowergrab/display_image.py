e04306e1e2cceb2371cbc5827831825f6426e2c0
Jack Rickards <jack.rickards@hotmail.co.uk>
2021-10-24 17:45:37 +0100

    added the code to display the image added some comments

--- selenium-flowergrab/display_image.py
+++ selenium-flowergrab/display_image.py
@@ -1,5 +1,36 @@
+#!/usr/bin/env python3
+
+import sys
+
+from PIL import Image
+from inky.inky_uc8159 import Inky
+
 def display_image():
     """puts the local image onto the screen"""
+    image_filepath = "flower_images/edited.png"
+
+    inky = Inky()
+    saturation = 0.5 # need to consider this because my post processing is also increasing sat.
+
+    # these could be useful later on to make this more dynamic
+    # so I will leave them for now
+
+    # if len(sys.argv) == 1:
+    #     print("""
+    # Usage: {file} image-file
+    # """.format(file=sys.argv[0]))
+    #     sys.exit(1)
+
+    # image = Image.open(sys.argv[1])
+
+    # if len(sys.argv) > 2:
+    #     saturation = float(sys.argv[2])
+
+    image = Image.open(image_filepath)
+
+    inky.set_image(image, saturation=saturation)
+    inky.show()
+
     return
 
 if __name__ == "__main__":
--- selenium-flowergrab/run.py
+++ selenium-flowergrab/run.py
@@ -3,6 +3,6 @@ from flower_grower import get_flower
 from display_image import display_image
 
 if __name__ == "__main__":
-    get_flower()
-    process_image()
-    display_image()
+    get_flower()     # get new flower image
+    process_image()  # process that into something which looks better on screen
+    display_image()  # put it on the screen
